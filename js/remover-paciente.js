a6cd8a7efe3e3cc74e76845048ebc7f5f1bedc38
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-05-28 12:46:59 -0300

    update

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,5 +3,6 @@ var pacientes = document.querySelectorAll('.paciente')
 pacientes.forEach(function(paciente){
     paciente.addEventListener('dblclick', function(){
         this.remove();
+        console.log(paciente)
     })
 })
