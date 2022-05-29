43b97afe31196725b88af6900a26aed8bebe19db
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-05-29 16:50:01 -0300

    updaste

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,10 +1,10 @@
-var tabela = document.querySelector("#tabela-pacientes");
+let tabela = document.querySelector("#tabela-pacientes");
 
-tabela.addEventListener("dblclick", function(event) {
-    event.target.parentNode.classList.add("fadeOut");
+tabela.addEventListener('dblclick', function(event) {
+    event.target.parentNode.classList.add('fadeOut');
 
     setTimeout(function() {
         event.target.parentNode.remove();
-    }, 500);
+    }, 1000);
 
 });
